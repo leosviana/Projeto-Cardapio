d2e92bd7e613cf885bc4db7ac5787ca925b37ae9
Leonardo S. Viana <leosiqueiraviana@gmail.com>
2024-06-06 07:21:26 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    fontFamily:{
+      'sans':['Robot','sans-serif']
+    },
     extend: {
       backgroundImage:{
         "home": "url('/assets/bg.png')"
